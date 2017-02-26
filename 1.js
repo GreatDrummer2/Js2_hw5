894f8fc2d9b0a778767573d644633640404c589d
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-26 14:01:17 +0200

    3

--- 1.js
+++ 1.js
@@ -2,7 +2,11 @@ function Clock(options){
   var elem;
   
   function getElem(){
-    if(!elem) render();
+    if(!elem){
+      render();
+      renderClockFace();
+      setTimeout(open, 1000);
+    } 
     return elem;
   }
 
@@ -16,10 +20,45 @@ function Clock(options){
     elem.appendChild(button);
   }
 
+  /*elem.onclick = function(event){
+    if(event.target.closest('clock')){
+      toggle();
+    }
+  }*/
+
+  function renderClockFace(){
+    var num = document.createElement('div');
+    num.className = 'num';
+    elem.appendChild(num); 
+    function date(){
+      var d = new Date();
+      var h = d.getHours();
+      if(h< 10) h = '0'+ h;
+      var m = d.getMinutes();
+      if(m< 10) m = '0'+ m;
+      var s = d.getSeconds();
+      s+=1;
+      if(s< 10) s = '0'+ s;
+      num.textContent = '['+ h + ':' + m + ':' + s + ']';
+    }
+    setInterval(date,1000);
+    elem.appendChild(num)
+  }
+
+   function open(){
+    var num = elem.getElementsByClassName('num');
+    console.log(num);
+    num.classList.add('open');
+   }
+
+   /*function toggle(){
+
+   }*/
+
   this.getElem = getElem;
 }
 
-var clock = new Clock({butTitle:'Alert'});
+var clock = new Clock({butTitle:'ALERT'});
 var elem = clock.getElem();
 document.body.appendChild(elem);
-    
+   
